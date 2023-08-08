c17ba691af24f216d2a55bf871c0cb7964c0e40b
premnath pichaimuthu <premwebdev+dev@gmail.com>
2023-08-08 09:32:33 +0530

    change

--- src/components/GalleryComp/OurGallery.js
+++ src/components/GalleryComp/OurGallery.js
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "./OurGallery.module.css"
 import imag1 from "./comp1.webp"
 import imag2 from "./comp2.webp"
@@ -5,15 +6,13 @@ import imag3 from "./comp3.webp"
 const OurGallery = () => {
   return (
     <div className={styles.gallery}>
-    <h2>Gallery Title</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     <div className={styles.imagegrid}>
       <div className="column1">
-        <img className="img0" {...imag1} alt="Ime 1" />
+        <Image className="img0" {...imag1} alt="Ime 1" />
       </div>
       <div className="column2">
-        <img className="img1" {...imag2} alt="Ime 2" />
-        <img className="img1" {...imag3} alt="Ime 3" />
+        <Image className="img1" {...imag2} alt="Ime 2" />
+        <Image className="img1" {...imag3} alt="Ime 3" />
       </div>
     </div>
   </div>
